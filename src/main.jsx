ae29a5c4247df811aa7097058897e6d1fc067b1f
Cinniecodes <lilthea65@gmail.com>
2024-11-29 11:29:14 -0800

    main

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './Style.css'
 import App from './App'
-// import './task.css'
-// import Drag_Drop from './Drag_Drop'
 
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
     <App/>
-    // {/* <Drag_Drop/> */}
-    
     
 //  </StrictMode>,
 )
